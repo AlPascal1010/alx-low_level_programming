48806d8ed6d8b48af1bef8537efff38855296aee
George Pascal <georgepascal1010@gmail.com>
2022-09-08 07:36:56 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+nclude <stdio.h>
+
+/**
+ * main -print the string in the put function
+ *
+ * Description: using the main function
+ * this program prints 
+ * return 0
+ */
+int main(void)
+{
+	char c;
+	int i;
+	long li:
+	long long lli;
+	float f;
+
+	print("size of a char: %ld byte(s)\n", sizeof(c));
+	printf("size of an int; %ld bytes(s)\n", sizeof(i));
+	printf("size of a long int; %ld bytes(s)\n", sizeof(li));
+	printf("size of a long long int; %id bytes(s)\n", sizeof(lli));
+	printf("size of a float: %ld byte(s)\n", sizeof(f));
+	return (0);
+}
