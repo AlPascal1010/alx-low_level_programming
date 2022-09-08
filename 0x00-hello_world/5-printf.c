09dfbb5eb64852619438b56fdb4ef9671c320b75
George Pascal <georgepascal1010@gmail.com>
2022-09-08 07:22:16 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - print the string in the put function
+ *
+ * Descripton: using the main function
+ * this program prints "Programing is like building a multilingual puzzle
+ * Return: 0
+ */
+int main(void)
+{
+	print("with proper grammar, but the outcome is a proper piece of art,\n");
+}
