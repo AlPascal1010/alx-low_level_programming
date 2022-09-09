e6e470bca104cdc965de1e02fb548e4aee9530fe
George Pascal <georgepascal1010@gmail.com>
2022-09-09 00:25:13 -0700

    c is interesting

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - entry point 
- * Return: always  0 (succes)
+ * main - ntry point 
+ * Return: Always 0 (succes)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a proper piece of art,");
+	printf("with proper grammar, but the outcome is a proper piece of art,\n");
 	return (0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,19 +5,18 @@
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	char c;
 	int i;
-	long li;;
+	long li;
 	long long lli;
 	float f;
 
-	printf("size of a char: %li byte(s)\n", sizeof(c));
-	printf("size of an int; %li bytes(s)\n", sizeof(i));
-	printf("size of a long int; %li bytes(s)\n", sizeof(li));
-	printf("size of a long long int; %li bytes(s)\n", sizeof(lli));
-	printf("size of a float: %li byte(s)\n", sizeof(f));
+	printf("size of a char: %lu  byte(s)\n", sizeof(c));
+	printf("size of an int; %lu bytes(s)\n", sizeof(i));
+	printf("size of a long int; %lu bytes(s)\n", sizeof(li));
+	printf("size of a long long int; %lu bytes(s)\n", sizeof(lli));
+	printf("size of a float: %lu byte(s)\n", sizeof(f));
 	return (0);
 }
