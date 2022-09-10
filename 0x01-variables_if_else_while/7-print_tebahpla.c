92225e5ee879fce6491029c29d53113824618f3d
George Pascal <georgepascal1010@gmail.com>
2022-09-10 14:41:56 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
+{
+	char g;
+
+	for (g = 'z'; g >= 'a'; g--)
+	{
+		putchar (g);
+	}
+
+	putchar ('\n');
+
+	return (0);
+
+}
