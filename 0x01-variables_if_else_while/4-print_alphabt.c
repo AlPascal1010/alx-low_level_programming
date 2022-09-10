d60e9965e800bda9af8474f4a6ade14b12d9591a
George Pascal <georgepascal1010@gmail.com>
2022-09-10 13:36:07 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+ int main(void)
+
+ {
+ 	char A;
+
+	for (A = 'a'; A <= 'z'; A++)
+	{
+		if (A == 'e')
+			continue;
+
+		else if (A == 'q')
+			continue;
+
+		putchar (A);
+	}
+
+	putchar ('\n');
+
+	return (0);
+ }
