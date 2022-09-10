53f80f1cc82a3c58b77cf5db20244684206a9eac
George Pascal <georgepascal1010@gmail.com>
2022-09-10 15:23:43 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-
 /**
- * main - ntry point 
+ * main - ntry point
+ *
  * Return: Always 0 (succes)
  */
 int main(void)
+
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
