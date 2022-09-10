c598b0a25c07275247cdbdd894625e4446894ba3
George Pascal <georgepascal1010@gmail.com>
2022-09-10 15:29:21 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,7 +13,7 @@ int main(void)
 	long long lli;
 	float f;
 
-	printf("Size of a char: %lu  byte(s)\n", sizeof(c));
+	printf("Size of a char: %lu byte(s)\n", sizeof(c));
 	printf("Size of an int; %lu bytes(s)\n", sizeof(i));
 	printf("Size of a long int; %lu bytes(s)\n", sizeof(li));
 	printf("Size of a long long int; %lu bytes(s)\n", sizeof(lli));
