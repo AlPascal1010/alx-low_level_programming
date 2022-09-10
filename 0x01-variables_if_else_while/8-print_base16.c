b103e7a1514fdcda10b11012be05020fbb7f7ee0
George Pascal <georgepascal1010@gmail.com>
2022-09-10 15:00:56 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+
+{
+	char b16;
+
+	for (b16 = 0; b16 = 16; b16++)
+	{
+		putchar(b16);
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
