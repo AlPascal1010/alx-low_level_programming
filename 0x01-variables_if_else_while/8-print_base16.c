16b5e0c3ce38a92aa681b43ff08859f16a3e6fb2
George Pascal <georgepascal1010@gmail.com>
2022-09-10 16:22:51 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,11 +9,11 @@ int main(void)
 {
 	char b16;
 
-	for (b16 = 0; b16 = 9; b16++)
+	for (b16 = 0; b16 < 9; b16++)
 	{
 		putchar(b16);
 	}
-	for (b16 = 'a'; b16 = 'f'; b16++)
+	for (b16 = 'a'; b16 <= 'f'; b16++)
 	{
 		putchar(b16);
 	}
