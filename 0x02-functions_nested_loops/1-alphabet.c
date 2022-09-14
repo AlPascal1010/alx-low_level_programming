3588a0c3f4776118b14c3d73160e2111d3ea40fa
George Pascal <georgepascal1010@gmail.com>
2022-09-14 00:27:33 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main"
+/**
+ * print_alphabet -function to print abc
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet
+{
+	char c;
+
+	for (c = 'a'; c <= 'z; c++)
+	{
+		_putchar(c);
+	}
+	putchar('\n');
+}
