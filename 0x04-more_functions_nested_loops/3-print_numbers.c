8f6f24abff558a9651338923187194c5616e1774
George Pascal <georgepascal1010@gmail.com>
2022-09-15 14:00:51 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,17 +4,16 @@
 /**
  * print_numvers - function that prints all the numbers from 0 to 9
  *
- * Return: Always 0
+ * Return: returns nothing
  */
-
 int print_numbers(void)
 {
 	int n;
 
 	for (n = 48; n < 58; n++)
 	{
-		putchar(n);
+		_putchar(n);
 	}
 
-	putchar('\n');
+	_putchar('\n');
 }
