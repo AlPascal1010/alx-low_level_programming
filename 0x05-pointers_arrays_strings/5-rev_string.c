18487a85e3a0c87807b74d788f75694ea809b0bd
George Pascal <georgepascal1010@gmail.com>
2022-09-19 16:11:50 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse string
+ * @s: string to reverse
+ *
+ * Return: Always 0
+ */
+
+void rev_string(char *s)
+{
+	char s;
+
+	putchar( *strrev(*s));
+
+	return (0);
+}
