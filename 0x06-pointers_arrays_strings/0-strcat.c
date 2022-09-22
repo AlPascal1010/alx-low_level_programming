159427a890ce7f0312092678695a9a52f1efe354
George Pascal <georgepascal1010@gmail.com>
2022-09-22 04:48:44 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,14 +9,10 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int i = 0;
-       int dest_len = 0;
+	char *save = dest;
 
-	while (dest[i++])
-		dest_len++;
-
-	for (i = 0; src[i]; i++)
-		dest[dest_len++] = src[i];
-
-	return (dest);
+	for (; *dest; ++s);
+	while ((*s++ = *src)  != '\0');
+	
+	return (dest)
 }
