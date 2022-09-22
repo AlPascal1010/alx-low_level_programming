bf2d324807cd150e7a7d2fd902df2372b298c3bd
George Pascal <georgepascal1010@gmail.com>
2022-09-22 05:04:25 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _strcat - concatenates string pointed to by @src to the end ofvthe string pointed to by @dest
  * @dest: string that will be appended
@@ -14,10 +13,10 @@ char *_strcat(char *dest, char *src)
 	int dest_len = 0;
 
 	while (dest[index++])
-		dest_len+;
+		dest_len++;
 
 	for (index = 0; src[index]; index++)
-		dest[dest_len+++] = src[index];
+		dest[dest_len++] = src[index];
 
 	return (dest);
 }
