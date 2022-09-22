bff0d70481a77ffa6907d0374ceebe270775ad13
George Pascal <georgepascal1010@gmail.com>
2022-09-22 16:34:21 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,38 +9,18 @@
 
 char *leet(char *str)
 {
-	int i = 0;
+	int i, j;
 
-	while (str[i] != '\0')
+	char a[] = "aAeEoOtTiL";
+	char b[] = "4433007711";
+
+	for (i = 0; *(s + i); i++)
 	{
-		str[i] = transform(str[i]);
-		i++;
-	}
-	return (srt);
-}
-
-/**
- * transform -helper function to map a letter with it's leet encoding
- * @x: char to be encoded
- *
- * Return: the encoded char
- */
-
-char transform(char x)
-{
-	char mapping_low[8] = {'0', 'l', '\0', 'e', 'a', '\0', '\0', 't'};
-	char mapping_upper[8] = {'O', 'L', '\0', 'E', 'A', '\0', '\0', 'T'};
-	int i = 0;
-	char replace = x;
-
-	while (i < 0)
-	{
-		if (x == mapping_low[i] || mapping_upper[i])
+		for (j = 0; j <= 9; j++)
 		{
-			replace = i + '0';
-			break;
+			if (a[j] == *(s + i))
+				*(s + i) = b[j];
 		}
-		i++;
 	}
-	return (replacement);
+	return (s);
 }
