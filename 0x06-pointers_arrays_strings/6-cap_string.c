57ba680fcbffe204eca70ff3ff85500a804d1cb5
George Pascal <georgepascal1010@gmail.com>
2022-09-22 16:09:25 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _strlen - returns the length of a string
- * @n: string
+ * @s: string
  * Return: Returns length as integers
  */
 
@@ -9,7 +9,7 @@ int _strlen(char *s)
 {
 	int len = 0;
 
-	while(*(s + len) != '\0')
+	while (*(s + len) != '\0')
 		len++;
 
 	return (len);
