c76d4e548de941db20ea37323c89bf06d771a5ec
George Pascal <georgepascal1010@gmail.com>
2022-09-26 14:20:38 -0700

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies @n bytes from the memory
+ *    area pointed to by @src into that pointed to by @dest.
+ * @dest: A pointer to the memory area to copy @src into.
+ * @src: tge source buffer to copy characters from
+ * @n: the number of bytes to copy from @src
+ */
+void *_memcpy(void *dest, const void *src, size_t n)
+{
+	unsigned int i;
+	unsigned char *destiny = dest;
+	const unsigned char *source = src;
+
+	for (i = 0, i < n; i++)
+		destiny[i] = source[i];
+
+	return (dest);
+}
