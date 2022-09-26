152abf6c384ec3924b80f68610b1534163ae3062
George Pascal <georgepascal1010@gmail.com>
2022-09-26 14:44:46 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a charcter in a string
+ * @s: String to check
+ * @c: charcter to check for
+ *
+ * Return: pointer to spot in s with c or null
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; *(s + i); i++)
+	{
+		if (*(s + i) == c)
+			return (s + i);
+	}
+	if (*(s + i) == c)
+		return (s + i);
+	return (0);
+}
