b94005443c2630a6feee924ef10bae38e82c854b
George Pascal <georgepascal1010@gmail.com>
2022-09-28 07:06:02 -0700

    3-factorial.c

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * factorial - returns the factorial of a given number
+ * @n: number to bubdactorized
+ *
+ * Return: the factorial of the number
+ */
+int factorial(int n)
+{
+	int nfactorial;
+
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (1);
+	nfactorial = factorial(n - 1);
+	return (n * nfactorial);
+}}
