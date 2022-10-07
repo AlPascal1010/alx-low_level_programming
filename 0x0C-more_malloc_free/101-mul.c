f9144439957daf0730581570dcdff6407552bc93
George Pascal <georgepascal1010@gmail.com>
2022-10-06 18:08:11 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,4 +1,6 @@
+#include <stdio.h>
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * main - multiplies two positive numbers
@@ -28,6 +30,6 @@ int main(int argc, char *argv[])
 		}
 	}
 	mul = atoi(argv[1]) * atoi(argv[2]);
-	printf("%lu\n", m);
+	printf("%lu\n", mul);
 	return (0);
 }
