97e6472d107f45a369fe945401bd2bd633f76409
George Pascal <georgepascal1010@gmail.com>
2022-10-10 18:34:58 -0700

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -18,7 +18,7 @@ void print_dog(struct dog *d)
 		printf("Name: %s\n", d->name);
 	
 	if (d->age < 0)
-		printf("Age: %f\n", d->age);
+		printf("Age: (nil)");
 	else
 		printf("Age: %f\n", d->age);
 	
