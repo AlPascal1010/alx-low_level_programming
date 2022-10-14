c3420396ec8cfbeaf7bed5d2b5205735605a9c14
George Pascal <georgepascal1010@gmail.com>
2022-10-14 02:52:22 -0700

    number 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,38 +9,30 @@
  */
 int main(int argc, char *argv[])
 {
-	int arg1, arg2, result;
-	char o;
-	int (*funv)(int, int);
+	int a = 0, b = 0, res;
+	char s;
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-
-	arg1 = atoi(argv[1]);
-	arg2 = atoi(argv[3]);
-
-	func = get_op_func(argv[2]);
-
-	if (!func)
+	
+	if (argv[2][1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	o = *argv[2];
-
-	if ((o == '/' || o == '%') && arg2 == 0)
+	s = argv[2][0];
+	if (s != '+' && s != '-' && s != '*' && s != '%')
 	{
 		printf("Error\n");
-		exit(100);
+		exit(99);
 	}
-
-	result = func(arg1, arg2);
-
-	printf("%d\n", result);
-
+	a = atoi(argv[1]);
+	b= atoi(argv[3]);
+	res = (get_op_func(argv[2]))(a, b);
+	printf("%d\n", res);
 	return (0);
 }
