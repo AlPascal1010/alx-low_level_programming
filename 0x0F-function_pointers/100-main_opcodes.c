df616261f6da09178c837329c7aa11af807a91fb
George Pascal <georgepascal1010@gmail.com>
2022-10-14 02:29:23 -0700

    100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
- * main - prints its own opvodes
+ * main - prints opcodes of a given machine
  * @argc: number of arguments
- * @argv: array of argumrnt
+ * @argv: argument vector
  *
- * Return: Always 0
+ * Return: 0.
  */
 int main(int argc, char *argv[])
 {
-	int byte, i;
-	char *arr;
+	int count, bytes;
 
 	if (argc != 2)
 	{
@@ -20,23 +18,18 @@ int main(int argc, char *argv[])
 	}
 
 	bytes = atoi(argv[1]);
-
 	if (bytes < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
-
-	arr = (char *)main;
-
-	for (i = 0; i < bytes; i++)
+	for (count = 0; count < bytes; count++)
 	{
-		if (i == bytes - 1)
-		{
-			printf("%02hhx\n", arr[i]);
-			break;
-		}
-		printf("%02hhx ", arr[i]);
+		printf("%02hhx", *((char *)main + count));
+		if (count < bytes - 1)
+			printf(" ");
+		else
+			printf("\n");
 	}
 	return (0);
 }
