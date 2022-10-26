bcb0129726e81f1920b4e8744be5ceb56a2a1b38
George Pascal <georgepascal1010@gmail.com>
2022-10-26 03:21:23 -0700

    10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -15,6 +15,9 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	unsigned int node;
 
 	if (copy == 0)
+		return (-1);
+
+	if (index == 0)
 	{
 		*head = (*head)->next;
 		free(copy);
