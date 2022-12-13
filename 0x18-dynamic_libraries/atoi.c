02fc52f7a481736f38d26c52a5154f0a8e690494
George Pascal <georgepascal1010@gmail.com>
2022-12-12 16:35:23 -0800

    atoi.c

--- 0x18-dynamic_libraries/atoi.c
+++ 0x18-dynamic_libraries/atoi.c
@@ -2,15 +2,15 @@
 
 /**
  * atoi - convert characters to int
- * res: output
- * str: text to be converted
+ * @res: output
+ * @str: text to be converted
  */
-int atoi(char* str)
+int atoi(char *str)
 {
 	int res = 0;
 
 	for (int i = 0; str[i] != '\0'; ++i)
 		res = res * 10 + str[i] - '0';
 
-	return res;
+	return (res);
 }
---
+++ 0x18-dynamic_libraries/ctypes.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * add - add 2 given integer
+ * @a: numbers to add
+ * @b: number to add
+ * Return: sum of add
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub subtrats a nuber form anther
+ * @a: number to be subtracted from
+ * @b: number
+ * Return: result of subtraction
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiplies two numbers
+ * @a: number 1
+ * @b: numberr 2
+ * Return: result of multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divide a number by another
+ * @a: number
+ * @b: numbers
+ * Return: result of division
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - return the remmainder affter a division of numbers
+ * @a: number
+ * @b: number
+ * Return: modulus of numbrs
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
