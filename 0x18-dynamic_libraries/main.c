d9bef997aac6e49f9211d6b96681217c1238d478
George Pascal <georgepascal1010@gmail.com>
2022-12-12 16:44:34 -0800

    main.c

--- 0x18-dynamic_libraries/main.c
+++ 0x18-dynamic_libraries/main.c
@@ -1,7 +1,8 @@
 #include "main.h"
+#include <stdio.h>
 
 int main(void)
 {
-	    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-	    return (0);
+	    _puts("%d\n", _strlen("my dynamic lib");
+	    return (EXIT_SUCCESS);
 }
