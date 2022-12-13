0aea0d1d01c864c4f74b305073e71606200eac37
George Pascal <georgepascal1010@gmail.com>
2022-12-12 16:55:41 -0800

    task 0

--- 0x18-dynamic_libraries/atoi.c
+++ 0x18-dynamic_libraries/atoi.c
@@ -1,16 +1,26 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * atoi - convert characters to int
- * @res: output
- * @str: text to be converted
+ * atoi - convert a string into an integer
+ * @s: the string tobe converted
+ * Retrun: int
  */
-int atoi(char *str)
+int atoi(char *s)
 {
-	int res = 0;
+	int sign = 1, i = 0;
+	unsigned int res = 0;
 
-	for (int i = 0; str[i] != '\0'; ++i)
-		res = res * 10 + str[i] - '0';
-
+	while (!(s[i] <= '9' && s[i] >= '0') && s[i] != '\0')
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (s[i] <= '9' && (s[i] >= '0' && s[i] != '\0'))
+	{
+		res = (res * 10) + (s[i] - '0');
+		i++;
+	}
+	res *= sign;
 	return (res);
 }
