ea6fe0706c4b5c0b2afb933e402f6082cfeb585a
George Pascal <georgepascaal1010@gmail.com>
2023-04-12 06:27:16 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,33 +1,28 @@
+#include "holberton.h"
 #include <stdio.h>
-#include "main.h"
 
 /**
- * print_diagonal - prints a diagonal
- * @n: parameter
- * Return: returns nothing
+ * print_diagonal - Print diagonal line dependent on the integer n.
+ * @n : The number of lines using '\' characters to use
+ * Return: Void.
  */
-
 void print_diagonal(int n)
 {
-	int len, space;
+	int i;
+	int spaces;
 
-	if (n > 0)
+	for (i = 0; i < n; i++)
 	{
-		for (len = 0; len < n; len++)
+		for (spaces = 0; spaces < i; spaces++)
 		{
-			for (space = 0; space < len; space)
-			{
-				putchar(' ');
-			}
-
-			putchar('\\');
-
-			if (len == (n - 1))
-			{
-				continue;
-			}
-			putchar('\n');
+			_putchar(' ');
 		}
+		_putchar('\\');
+		_putchar('\n');
 	}
-	putchar('\n');
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
 }
