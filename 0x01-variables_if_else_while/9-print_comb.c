e8f8508d4bfc05dec75993b6b6a1b3ff9d0b1b3e
George Pascal <georgepascaal1010@gmail.com>
2023-04-12 11:58:47 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,22 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
-
 {
-	int num;
+	int d;
 
-	for (num = '0'; num < '10'; num++)
+	for (d = '0'; d <= '9'; d++)
 	{
-		putchar(num);
-
-		putchar(',');
-		putchar(' ');
+		putchar(d);
+		if (d != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
-
 	putchar('\n');
 
 	return (0);
