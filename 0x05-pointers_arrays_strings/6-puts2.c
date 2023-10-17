14ac9b4e9e52872d25a65cd5952e91284669b7ef
George Pascal <georgepascal1010@gmail.com>
2023-10-17 02:21:49 +0100

    7-puts_half.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- *  * puts2 - prints every other character of a string.
- *   *@str: the string to use.
- *    */
+ * puts2 - prints every other character of a string.
+ * @str: the string to use.
+ */
 
 void puts2(char *str)
 {
--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,36 +1,17 @@
 #include "main.h"
-
 /**
- * puts_half - main function.
- * @str: Pointer, the string of numbers to print.
- *
- * Description: This function prints half of a string.
- *
- * Return: none.
+ * puts_half - prints the second half of a string.
+ * @str: string to use.
  */
 
 void puts_half(char *str)
 {
-	int n;
-	int i;
-
-	while (str[n])
+	int length, n, i;
+	for (length = 0; str[length] != '\0'; length++)
 	{
-		n++;
 	}
-
-	if (n % 2 == 1)
-	{
-		i = (n - 1) / 2;
-		i = n - i;
-	}
-
-	else
-	{
-		i = n / 2;
-	}
-
-	for (; str[i]; i++)
+	n = (length - 1) / 2;
+	for (i = n + 1; str[i] != '\0'; i++)
 	{
 		_putchar(str[i]);
 	}
