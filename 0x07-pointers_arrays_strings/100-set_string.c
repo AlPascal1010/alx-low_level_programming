288dcbca76dcca5cc51a7179f2349f3155a807b7
George Pascal <georgepascal1010@gmail.com>
2023-10-23 17:01:49 +0100

    100-set_string.c

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,11 +1,9 @@
 #include "main.h"
 
 /**
- * ser_string - set the value of a pointer to a char
- * @s: pointe to change
- * @to: string to change pointer to
- *
- * Return: void
+ * set_string - sets the value of a pointer to a char
+ * @s: pointer to pointer we need to set to
+ * @to: string to set
  */
 void set_string(char **s, char *to)
 {
