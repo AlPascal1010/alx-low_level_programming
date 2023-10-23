816979f61958c56bbc90f7d6e021d9eb709c2107
George Pascal <georgepascal1010@gmail.com>
2023-10-23 16:57:51 +0100

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,24 +1,21 @@
-/*
- * file: 0-memset
- * Auth: not
- */
-
 #include "main.h"
 
 /**
- * _memset - fills a memory block with a constant byte
- * @s: addtess to memory block
- * @b: char to be used
- * @n: number of bytes to be used
+ * *_memset - fills memory with a constant byte
+ * @s: memory area to be filled
+ * @b: char to copy
+ * @n: number of times to copy b
  *
- * Return: pointer to the memory block
+ * Return: pointer to the memory area s
  */
-
 char *_memset(char *s, char b, unsigned int n)
 {
-	while (n)
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
 	{
-		s[n - 1] = b;
+		s[i] = b;
 	}
+
 	return (s);
 }
